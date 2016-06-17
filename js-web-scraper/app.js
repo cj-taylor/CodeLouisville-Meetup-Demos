4ed0fe1445417a5e1e99206af465c3f3ea140ad0
CJ Taylor <cjtayl05@gmail.com>
2016-06-17 01:53:51 -0400

    Added track options and selector objects for use by scraper.

--- js-web-scraper/app.js
+++ js-web-scraper/app.js
@@ -2,3 +2,16 @@
 // PLAN: use node.js to scrape the Treehouse page to get the track modules and their links then get various metadata 
 
 var sjs = require('scraperjs');
+
+var options = {
+    track : {
+        URL: 'https://teamtreehouse.com/tracks/full-stack-javascript'
+    }
+}
+
+var selectors = {
+    track: {
+        name: '#track-meta h2',
+        estimate: '.card-estimate'
+    }
+};
