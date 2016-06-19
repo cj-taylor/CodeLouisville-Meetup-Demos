479b6d543214c120eb6be4c1e4143dda532030de
CJ Taylor <cjtayl05@gmail.com>
2016-06-19 17:09:32 -0400

    Added initial test config for wallaby/mocha/expect.

--- treehouse-js-web-scraper/src/app.js
+++ treehouse-js-web-scraper/src/app.js
@@ -1,6 +1,3 @@
-// PROBLEM: We need an easy way to parse the Treehouse track for all the modules and get their data
-// PLAN: use node.js to scrape the Treehouse page to get the track modules and their links then get various metadata 
-
 var sjs = require('scraperjs');
 
 var options = {
