0e36c46ec097cbbd49a58c1d516b519797d571e1
CJ Taylor <cjtayl05@gmail.com>
2016-06-19 22:45:57 -0400

    Added Treehouse/TrackOperations and applicable tests.

---
+++ treehouse-js-web-scraper/src/Treehouse/TrackOperations.js
@@ -0,0 +1,20 @@
+var jsonAccessor = require('../JsonAccessor');
+
+exports.getTrackMetadata = function (index, trackObject, pathsToData) {
+    var estimate = _getEstimate(trackObject, pathsToData);
+    var trackName = _getTrackName(trackObject, pathsToData);
+
+    return {
+        id: index,
+        name: trackName,
+        estimate: estimate
+    };
+};
+
+function _getEstimate(obj, pathsToData) {
+    return jsonAccessor.getPropertyValue(obj, pathsToData.estimate) || jsonAccessor.getPropertyValue(obj, pathsToData.estimateFallback);
+}
+
+function _getTrackName(obj, pathsToData) {
+    return jsonAccessor.getPropertyValue(obj, pathsToData.trackName);
+}
---
+++ treehouse-js-web-scraper/test/Treehouse/TrackOperations.Spec.js
@@ -0,0 +1,51 @@
+var expect = require('expect'),
+    TrackOperations = require('../../src/Treehouse/TrackOperations'),
+    JsonAccessor = require('../../src/JsonAccessor');
+
+describe('Treehouse', function () {
+
+    describe('TrackOperations', function () {
+        var testObject;
+
+        beforeEach(function (done) {
+            TrackOperations = require('../../src/Treehouse/TrackOperations');
+            JsonAccessor = require('../../src/JsonAccessor');
+
+            testObject = {
+                trackId: 1, 
+                track: {
+                    data: {
+                        name: "Test Track",
+                        estimate: "2 hours"
+                    }
+                },
+                pathsToData: {
+                    estimate: "data.estimate",
+                    trackName: "data.name"
+                }
+            };            
+            done();
+        });
+
+        it('should return track information', function (done) {
+            var expectedResult = { 
+                id: testObject.trackId,
+                estimate: testObject.track.data.estimate,
+                name: testObject.track.data.name 
+            };
+
+            var result = TrackOperations.getTrackMetadata(
+                testObject.trackId, 
+                testObject.track,
+                testObject.pathsToData);
+
+            expect(result.id).toBe(expectedResult.id);
+            expect(result.name).toBe(expectedResult.name);
+            expect(result.estimate).toBe(expectedResult.estimate);
+
+            done();
+        });
+
+    });
+
+});
